ebd32457d64cf1d0e9651c4ea1e0203c67f4c204
Sagar <165837591+Sagar01GitHub@users.noreply.github.com>
2024-07-18 03:42:22 +0530

    Add files via upload
    commit file stringclassdemo

---
+++ stringclassdemo.java
@@ -0,0 +1,31 @@
+package CoreJava;
+
+public class stringclassdemo {
+
+	public static void main(String[] args) {
+		
+		
+		//String = it is one of the prebuild class in java
+	   /*1.String literal
+	     2.by creating object of string*/
+		
+		String a = " javatraining";
+		
+		System.out.println(a.charAt(2));
+		System.out.println(a.indexOf("e"));
+		System.out.println(a.substring(3, 6));
+		System.out.println(a.substring(3));
+		System.out.println(a.concat(" with selenium"));
+		System.out.println(a.trim());
+		System.out.println(a.trim().toUpperCase());
+	  //a.toLowerCase();
+		
+		//split
+		String arr[] = a.split("t");
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(a.replace("t", "s"));
+	}
+
+}
